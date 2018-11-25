f2fdb012082af2f75ae69181ca55d25406617bbc
pichunying <2339348937@qq.com>
2018-11-25 09:08:02 +0800

    Update EntryForm.php

--- EntryForm.php
+++ EntryForm.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 namespace app\models;
@@ -18,3 +19,25 @@ class EntryForm extends Model
         ];
     }
 }
+=======
+<?php
+
+namespace app\models;
+ 
+use Yii;
+use yii\base\Model;
+
+class EntryForm extends Model
+{
+    public $name;
+    public $email;
+
+    public function rules()
+    {
+        return [
+            [['name','email'],'required'],
+            ['email','email'],
+        ];
+    }
+}
+>>>>>>> c7894b054e0f7eae26687c5023d89436be0ec2b9
